24a59c4cf91aed81158a21cff07bca4a591e3c59
Matheus Albino <matheusjva@gmail.com>
2019-04-18 18:20:33 -0300

    Ao pressionar tecla 1 no banner ativo, a pagina é redirecionada para o index.

--- resources/views/includeHtmls.blade.php
+++ resources/views/includeHtmls.blade.php
@@ -1,36 +1,48 @@
-@extends('app');
-@section('conteudo')
-        
-       
+    <script>
+        function keyPressed(evt){
+            evt = evt || window.event;
+            var key = evt.keyCode || evt.which;
+            return String.fromCharCode(key); 
+        }
+
+        document.onkeypress = function(evt) {
+            var str = keyPressed(evt);
             
-       
-        
-        @php
-           $timeInitial = 0;
-           $timeFinaly = 0;
-        @endphp
-        @foreach($bannersItens as $itemBanner)
-                @php
-                    $nameItem = explode('.blade.php', $itemBanner['filename']);
-                    $name = $nameItem[0];
-                    $timeFinaly = $timeFinaly + $itemBanner['seconds'];
-                @endphp
-                <div id={{$name}} style="display:none">
-                    @include('htmls.'. $nameItem[0])
-                </div>
+            if(str == '1')
+            window.location.href = "http://localhost:8000";
+        };
+    </script>
+    @php
+        $timeInitial = 0;
+        $timeFinaly = 0;
+    @endphp
+    @foreach($bannersItens as $itemBanner)
+            @php
+                $nameItem = explode('.blade.php', $itemBanner['filename']);
+                $name = $nameItem[0];
+                $timeFinaly = $timeFinaly + $itemBanner['seconds'];
+            @endphp
+            <div id={{$name}} style="display:none">
+                @include('htmls.'. $nameItem[0])
+            </div>
 
-                <script>
-                    setTimeout( function() {
-                        $("#{{$name}}").show()
-                    },  {{$timeInitial}});
-                    setTimeout( function() {
-                        $("#{{$name}}").hide()
-                    },  {{ $timeFinaly }} );
-                </script>
+            <script>
+                setTimeout( function() {
+                    $("#{{$name}}").show()
+                },  {{$timeInitial}});
+                setTimeout( function() {
+                    $("#{{$name}}").hide()
+                },  {{ $timeFinaly }} );
+            </script>
 
-                @php
-                    $timeInitial = $timeInitial + $itemBanner['seconds'];
-                @endphp
-        @endforeach
+            @php
+                $timeInitial = $timeInitial + $itemBanner['seconds'];
+            @endphp
+    @endforeach
 
-@endsection
+    <script>
+        setTimeout( function(){
+            window.location.reload()
+            }, {{ $timeFinaly}});
+    </script>
+
