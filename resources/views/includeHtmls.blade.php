120549b2c97061b7b2d3dab0e72a5fddc58898fa
Matheus Albino <matheusjva@gmail.com>
2019-05-09 18:04:42 -0300

    Implementado o armazenamento no s3.

--- resources/views/includeHtmls.blade.php
+++ resources/views/includeHtmls.blade.php
@@ -17,16 +17,18 @@
         $timeFinaly = 0;
     @endphp
     @foreach($bannersItens as $itemBanner)
+
             @php
                 $nameItem = explode('.blade.php', $itemBanner['filename']);
                 $name = $nameItem[0];
                 $timeFinaly = $timeFinaly + $itemBanner['seconds'];
                 $html = \Illuminate\Support\Facades\Storage::get($itemBanner['filename']);
 
-                $htmlfinal = trim($html, '\"')
             @endphp
             <div id={{$name}} style="display:none">
-                {{$htmlfinal}}
+                @php
+                    echo $html;
+                @endphp
             </div>
 
             <script>
@@ -43,11 +45,11 @@
             @endphp
     @endforeach
 
-{{--    <script>--}}
-{{--        setTimeout( function(){--}}
-{{--            window.location.reload()--}}
-{{--            }, {{ $timeFinaly}});--}}
-{{--    </script>--}}
+    <script>
+        setTimeout( function(){
+            window.location.reload()
+            }, {{ $timeFinaly}});
+    </script>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
